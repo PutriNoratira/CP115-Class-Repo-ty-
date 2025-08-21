f2dd18cec4b3afdd75cf0cceb79b792469d2041f
Tyra. <ptrnrtrsyaa@gmail.com>
2025-08-21 20:48:00 +0800

    update

---
+++ labs/lab07/exercise5/exercise5.py
@@ -0,0 +1,12 @@
+import personal_data
+
+print ("===MY PROFILE===")
+print (f"Full Name: {personal_data.name}\tAge: {personal_data.age}")
+print (f"Height(cm): {personal_data.height_in_cm}\t\t\t\t\t\tWeight(kg): {personal_data.weight_in_kg}")
+print (f"Phone Number: {personal_data.phone_number}\t\t\t\tEmail: {personal_data.email_address}")
+print (f"Full Name in uppercase: {personal_data.name_upper}")
+print (f"Full Name in lowercase: {personal_data.name_lower}")
+print (f"Full Name lenth: {personal_data.name_length}")
+print (f"City Name in uppercase: {personal_data.city_upper}")
+print (f"Address: {personal_data.street_address}, {personal_data.city}, {personal_data.postcode}")
+print (f"Address Length :{personal_data.address_length}")
---
+++ labs/lab07/exercise5/personal_data.py
@@ -0,0 +1,18 @@
+import random
+import math
+
+name = "Putri Noratira Natasya Binti Hairul Izuan"
+age = 18
+height_in_cm = 150
+weight_in_kg = 50
+phone_number = "0196406826"
+email_address = "ptrnrtrsyaa@gmail.com"
+street_address = "Tawau Ria Height"
+city = "Tawau"
+postcode = "91005"
+name_upper = name.upper()
+name_lower = name.lower()
+name_length = len(name)
+city_upper = city.upper()
+address = street_address , city , postcode
+address_length = len(address)
