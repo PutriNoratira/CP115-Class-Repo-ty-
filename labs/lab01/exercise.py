c708c890931550c1ca441b5cffca256f9411b74e
Tyra. <ptrnrtrsyaa@gmail.com>
2025-09-13 23:50:20 +0800

    update

--- labs/lab01/exercise.py
+++ labs/lab01/exercise.py
@@ -1,5 +1,3 @@
 # labs/lab01/exercise.py
 # Starter code for Lab 01
-print("Hello everyone, Lab 01, di sini ada syafiq imut bgt lagi!")
-
-
+print("Hello, Lab 01")
--- labs/lab09/exercise5/exercise5.py
+++ labs/lab09/exercise5/exercise5.py
@@ -5,5 +5,42 @@ customer_age = int(input())
 
 # TODO: Your code here
 
+# Main course prices
+if main_course == "Chicken":
+    main_course_cost = 10.00
+elif main_course == "Beef":
+    main_course_cost = 12.00
+elif main_course == "Fish":
+    main_course_cost = 11.00
+else:
+    main_course_cost = 0.00
+
+# Drink prices
+if drink == "Soft Drink":
+    drink_cost = 2.00
+elif drink == "Coffee":
+    drink_cost = 3.00
+else:
+    drink_cost = 0.00
+
+# Dessert prices
+if dessert == "Ice Cream":
+    dessert_cost = 4.00
+elif dessert == "Cake":
+    dessert_cost = 5.00
+else:
+    dessert_cost = 0.00
+
+# Total food cost
+total_food_cost = main_course_cost + drink_cost + dessert_cost
+
+# Add 10% service charge
+final_bill = total_food_cost * 1.10
+
+# Discounts
+if customer_age > 60:
+    final_bill *= 0.85   # 15% off
+elif customer_age < 18:
+    final_bill *= 0.90   # 10% off
 
 print(f"{final_bill:.2f}")
