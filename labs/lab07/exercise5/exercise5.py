8e44f660ae5f8045314457becdbd8c787d834f40
Tyra. <ptrnrtrsyaa@gmail.com>
2025-09-22 11:23:45 +0800

    update

--- labs/lab07/exercise5/exercise5.py
+++ labs/lab07/exercise5/exercise5.py
@@ -6,7 +6,7 @@ print (f"Height(cm): {personal_data.height_in_cm}\t\t\t\t\t\tWeight(kg): {person
 print (f"Phone Number: {personal_data.phone_number}\t\t\t\tEmail: {personal_data.email_address}")
 print (f"Full Name in uppercase: {personal_data.name_upper}")
 print (f"Full Name in lowercase: {personal_data.name_lower}")
-print (f"Full Name lenth: {personal_data.name_length}")
+print (f"Full Name length: {personal_data.name_length}")
 print (f"City Name in uppercase: {personal_data.city_upper}")
 print (f"Address: {personal_data.street_address}, {personal_data.city}, {personal_data.postcode}")
 print (f"Address Length :{personal_data.address_length}")
